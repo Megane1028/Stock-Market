3da5b1a536a65fa72e1d0c4297f38645c5273ce6
Yanjing-Alen <Yanjinggpt@hotmail.com>
2024-07-02 19:01:57 +0800

    Add files via upload

---
+++ zhishu.php
@@ -0,0 +1,7 @@
+#!/usr/bin/env php
+<?php
+define('APP_PATH', __DIR__ . '/application/');
+define('BIND_MODULE','push/Zhishu');
+// 加载框架引导文件
+require __DIR__ . '/thinkphp/start.php';
+//天美社区源码网 timibbs.net timibbs.com timibbs.vip
